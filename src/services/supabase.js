19fa5637fb904c112ff238a31b1b0a9254492240
Afif Bin Kamrul <afifbinkamrul@gmail.com>
2023-12-25 02:40:45 -0500

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -5,6 +5,7 @@ export const supabaseUrl = "https://jzwetzccxtnugbvxppdm.supabase.co";
 let supabaseKey;
 if (import.meta.env.NETLIFY === "true") {
   supabaseKey = process.env.VITE_SUPABASE_KEY;
+  console.log(supabaseKey);
 } else {
   supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 }
